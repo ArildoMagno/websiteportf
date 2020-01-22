e80132416ff662319e0f5a65aff908acbd9c0b45
Arildo <0040117@academico.ifmg.edu.br>
2020-01-22 00:32:04 -0300

    Updates

---
+++ precache-manifest.f32f36aefcb17ff30ef5542c34dce19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15128c5845fc9509531942147a794f0",
+    "url": "/deployteste/index.html"
+  },
+  {
+    "revision": "98d1eba16cafe6dda7d9",
+    "url": "/deployteste/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8f91d284855eb80160f",
+    "url": "/deployteste/static/js/2.8b84508d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deployteste/static/js/2.8b84508d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98d1eba16cafe6dda7d9",
+    "url": "/deployteste/static/js/main.1cb7bb30.chunk.js"
+  },
+  {
+    "revision": "94e7944856e8dc328ce5",
+    "url": "/deployteste/static/js/runtime-main.f1780dd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployteste/static/media/logo.5d5d9eef.svg"
+  }
+]);
